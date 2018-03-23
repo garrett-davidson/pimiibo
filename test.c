bcbae7ce4c8d9ca9b8511efaddce65bf4c006377
Garrett Davidson <davidson.garrettm@gmail.com>
2018-03-23 01:11:25 +0000

    Generalize reading bin file

--- test.c
+++ test.c
@@ -25,6 +25,7 @@ static void print_hex(const uint8_t *pbtData, const size_t szBytes)
 }
 
 void initializeNFC() {
+  printf("Initializing NFC adapter\n");
   nfc_init(&context);
 
   if (!context) {
@@ -46,21 +47,32 @@ void initializeNFC() {
   printf("NFC reader: opened\n");
 }
 
-void readDecryptedBin(const char *path) {
-  FILE *decryptedBinFile = fopen(path, "r");
-  if (!decryptedBinFile) {
+void readFileIntoBuffer(const char *path, uint8_t *buffer, size_t size) {
+  FILE *file = fopen(path, "r");
+  if (!file) {
     fprintf(stderr, "Could not open %s\n", path);
     exit(1);
   }
 
-  if (BIN_SIZE != fread(decryptedBin, 1, BIN_SIZE, decryptedBinFile)) {
-    fprintf(stderr, "Read incorrect number of bytes from bin file\n");
+  if (size != fread(buffer, 1, size, file)) {
+    fprintf(stderr, "Read incorrect number of bytes from file: %s\n", path);
     exit(1);
   }
+
+}
+
+void readDecryptedBin(const char *path) {
+  printf("Reading bin file\n");
+  readFileIntoBuffer(path, decryptedBin, BIN_SIZE);
+}
+
+void readKey(const char *path) {
+  printf("Reading key file\n");
+  readFileIntoBuffer(path, key, KEY_SIZE);
 }
 
 void readTag() {
-  printf("Scan tag\n");
+  printf("***Scan tag***\n");
   const nfc_modulation nmMifare = {
     .nmt = NMT_ISO14443A,
     .nbr = NBR_106,
@@ -68,10 +80,21 @@ void readTag() {
 
   if (nfc_initiator_select_passive_target(pnd, nmMifare, NULL, 0, &nt) > 0) {
     printf("Read UID: ");
-    print_hex(nt.nti.nai.abtUid, nt.nti.nai.szUidLen);
+    int uidSize = nt.nti.nai.szUidLen;
+    print_hex(nt.nti.nai.abtUid, uidSize);
 
+    if (UID_SIZE != uidSize) {
+      fprintf(stderr, "Read wrong size UID\n");
+      exit(1);
+    }
 
+    for (int i = 0; i < UID_SIZE; i++) {
+      uid[i] = nt.nti.nai.abtUid[i];
+    }
   }
+}
+
+void encryptBin() {
 
 }
 
@@ -80,13 +103,15 @@ void printUsage() {
 }
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
+  if (argc != 3) {
     fprintf(stderr, "Incorrect number of arguments\n");
     printUsage();
     exit(1);
   }
 
-  readDecryptedBin(argv[1]);
+  readDecryptedBin(argv[2]);
+  readKey(argv[1]);
   initializeNFC();
   readTag();
+  encryptBin();
 }
